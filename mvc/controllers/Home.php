3240e998b0cdd8261416cc18e0dc84d629f8271f
Nam Luu <nam.luuduc@gmail.com>
2021-03-09 07:26:37 +0700

    init

---
+++ mvc/controllers/Home.php
@@ -0,0 +1,34 @@
+<?php
+
+// http://localhost/live/Home/Show/1/2
+
+class Home extends Controller
+{
+
+    function index()
+    {
+        echo 'Hello World';
+    }
+    // Must have SayHi()
+    function SayHi()
+    {
+        $teo = $this->model("SinhVienModel");
+        echo $teo->GetSV();
+    }
+
+    function Show($a, $b)
+    {        
+        // Call Models
+        $teo = $this->model("SinhVienModel");
+        $tong = $teo->Tong($a, $b); // 3
+
+        // Call Views
+        $this->view("aodep", [
+            "Page"=>"news",
+            "Number"=>$tong,
+            "Mau"=>"red",
+            "SoThich"=>["A", "B", "C"],
+            "SV" => $teo->SinhVien()
+        ]);
+    }
+}
